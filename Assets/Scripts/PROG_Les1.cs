c7ca7d90d29490f17eaa6dc6f0b767e3ee810219
Schrever <jbb090410@gmail.com>
2026-09-17 12:59:12 +0200

    dingus

---
+++ Assets/Scripts/PROG_Les1.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class PROG_les1 : MonoBehaviour
+{
+
+    string Naam = "Erwin";
+    int Score = 1000;
+    bool Alive = true;
+
+    static void Main(string[] args)
+    {
+        Debug.Log("hello");
+    }
+
+    void Update()
+    {
+        
+    }
+}
